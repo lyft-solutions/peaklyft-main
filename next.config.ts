2f64856555aafc4a5759a2164e1535c047e85b97
mukesh <mukesh.kumar@peaklyft.top>
2025-12-18 14:23:36 +0530

    chore: migrate images.domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "cdn-icons-png.flaticon.com",
-      "upload.wikimedia.org",
-      "www.antwalk.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.antwalk.com",
+      },
     ],
   },
 };
 
 export default nextConfig;
-
