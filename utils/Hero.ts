3c0a987dab2824a56b1a30e5ba667e23f061ed57
mukesh <mukesh.kumar@peaklyft.top>
2025-12-18 15:27:52 +0530

    18-12

--- utils/Hero.ts
+++ utils/Hero.ts
@@ -14,7 +14,7 @@ export const heroSections = {
     description:
       " Peaklyft <b>Business Capability Platform</b> empowers your workforce to achieve your business goals by driving organization-wide capability building through end-to-end Capability Academy​",
     button: { label: "Schedule a Demo!" },
-    heroImage: "/hero.png",
+    heroImage: "/Hero.png",
   } as const,
 
   growth: {
