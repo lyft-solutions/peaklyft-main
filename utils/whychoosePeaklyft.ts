79b43323b6df39576b3ea12b58a178ac1e5a80bf
mukesh <mukesh.kumar@peaklyft.top>
2025-12-18 18:35:48 +0530

    updated whychooseAntwalk Component

--- utils/whychoosePeaklyft.ts
+++ utils/whychoosePeaklyft.ts
@@ -1,6 +1,5 @@
 export const whyChooseAntWalkData = {
-  title: "Why Choose Peaklyft for Web Development?",
+  title: "Why Choose Peaklyft for  Development?",
   description:
     "PeakLyft delivers fast, scalable, and secure web applications using modern technologies like React and Next.js. We focus on performance, SEO, and long-term maintainability to help your business grow.",
 };
-
