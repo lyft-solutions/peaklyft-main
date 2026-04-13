b26ff4a8effc48f352a64597635b5b4eb62b8528
aadesh sain <aajad.sain@peaklyft.top>
2026-04-13 14:45:43 +0530

    change name for mobile image

--- src/app/solutions/mobile-app/page.tsx
+++ src/app/solutions/mobile-app/page.tsx
@@ -21,7 +21,7 @@ export default function SalesAcademyPage() {
         <AcademyTabs
           title="Explore Mobile App Development Offerings"
           tabs={salesAcademyTab}
-          image="/spider-charts/Mobile-app.jpg"
+          image="/spider-charts/Mobile-App.jpg"
         />
         <WhatsNewCards data={whatsNewDefaultData} />
       </div>
